06e8ad02beca8545e41940a6aae9975a9aee6101
h3yon <h3yon@wemakeprice.com>
2021-09-23 02:01:32 +0900

    feat: news xlsx

--- news.py
+++ news.py
@@ -1,3 +1,4 @@
+from openpyxl import Workbook
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
@@ -11,11 +12,18 @@ soup = BeautifulSoup(req, 'html.parser')
 
 articles = soup.select('#main_pack > section.sc_new.sp_nnews._prs_nws > div > div.group_news > ul > li')
 
+wb = Workbook()
+ws1 = wb.active
+ws1.title = "articles"
+ws1.append(["제목", "링크", "신문사"])
+
 for article in articles:
     title = article.select_one('div.news_area > a').text
     url = article.select_one('div.news_area > a')['href']
     comp = article.select_one('a.info.press').text.split(' ')[0].replace('언론사', '')
     # div.news_area > div > div.info_group > a > span
-    print(comp)
+
+    ws1.append([title, url, comp])
 
 driver.quit()
+wb.save(filename='articles.xlsx')
