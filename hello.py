e28ae1f0466bf5120aa31530033697c832b4006b
h3yon <h3yon@wemakeprice.com>
2021-09-23 00:37:28 +0900

    first commit

---
+++ hello.py
@@ -0,0 +1,24 @@
+# 변수, 자료형, 조건문, 반복문, 기타 라이브러리
+
+a = 2
+b = 3
+print(a + b) # 5
+
+# 문자열 더하기
+
+first_name = 'bumkyu'
+last_name = 'lee'
+
+print(first_name + last_name) #bumkyulee
+
+# 숫자 + 문자열
+
+print(a + first_name) # error
+print(str(a) + first_name) # 2bumkyu
+
+# list
+a_list = ['사과', '감', '배']
+b_list = ['영희', '철수']
+
+print(b_list[0])
+
