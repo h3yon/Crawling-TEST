c0b7b716b8b7e7a374913195f22181a7c0293a79
h3yon <h3yon@wemakeprice.com>
2021-09-23 02:26:57 +0900

    feat: word cloud

--- wrpractice.py
+++ wrpractice.py
@@ -1,8 +1,13 @@
+from wordcloud import WordCloud
 
 text = ''
-with open("test.txt", "r", encoding="utf-8") as f:
+with open("kakao.csv", "r", encoding="utf-8") as f:
     lines = f.readlines()
     for line in lines:
         text += line
 
-print(line)
+print(text)
+
+wc = WordCloud(font_path='/System/Library/Fonts/AppleSDGothicNeo.ttc', background_color="white", width=600, height=400)
+wc.generate(text)
+wc.to_file("result.png")
