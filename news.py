6121889ce9cae7604cfaddaf1612d0072bb90d46
h3yon <h3yon@wemakeprice.com>
2021-09-23 01:58:48 +0900

    test: openpyxl

---
+++ news.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+driver = webdriver.Chrome('./chromedriver')
+
+url = "https://search.naver.com/search.naver?where=news&sm=tab_jum&query=추석"
+
+driver.get(url)
+req = driver.page_source
+soup = BeautifulSoup(req, 'html.parser')
+
+articles = soup.select('#main_pack > section.sc_new.sp_nnews._prs_nws > div > div.group_news > ul > li')
+
+for article in articles:
+    title = article.select_one('div.news_area > a').text
+    url = article.select_one('div.news_area > a')['href']
+    comp = article.select_one('a.info.press').text.split(' ')[0].replace('언론사', '')
+    # div.news_area > div > div.info_group > a > span
+    print(comp)
+
+driver.quit()
